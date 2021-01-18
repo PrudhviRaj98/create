f688c13f90387b6fb5ec959adec6a38601d15706
prudhvu raj <rprudhvi645@gmail.com>
2021-01-18 21:06:07 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [apiData,_setApiData] = React.useState({})
+
+  const FetchData = ()=>{
+   axios.get('https://api.github.com/orgs/octokit/repos').then(data=>{
+      _setApiData({...data.data})
+   })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1>{JSON.stringify(apiData)}</h1>
+  <button onClick={FetchData}>Fetch DAta</button>
     </div>
   );
 }
